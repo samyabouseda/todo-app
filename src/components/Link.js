f41e851e61bd5a0c7fa08581f6b522df19b1aeb0
samyabouseda <samy@abouseda.ch>
2019-09-30 15:31:50 +0200

    Add item crossing and visibility filters

---
+++ src/components/Link.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Link = ({ active, children, onClick }) => {
+    if (active) {
+        return <span>{children}</span>
+    }
+
+    const handleClick = event => {
+        event.preventDefault()
+        onClick()
+    }
+
+    return (
+        <a
+            href=""
+            onClick={handleClick}
+        >
+            {children}
+        </a>
+    )
+}
+
+Link.propTypes = {
+    active: PropTypes.bool.isRequired,
+    children:  PropTypes.node.isRequired,
+    onClick: PropTypes.func.isRequired
+}
+
+export default Link
